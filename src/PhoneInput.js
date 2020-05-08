1df7f3a8ca09df202b6e4c0dd14c72cbc8d5a1ba
Adesina-123 <adesinaoladimeji92@gmail.com>
2020-05-08 16:41:33 +0200

    add flag to countries

---
+++ src/PhoneInput.js
@@ -0,0 +1,13 @@
+// import React from 'react'
+
+// function PhoneInput() {
+//     return (
+//         <div>
+//             <select name="" id="">
+//                 <option value=""></option>
+//             </select>
+//         </div>
+//     )
+// }
+
+// export default PhoneInput
